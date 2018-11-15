b75a28cf0e7eb5a1b5088901257a4021ce5d2816
Ayush Kaushik <anon.programmer305@gmail.com>
2018-11-15 14:50:59 -0500

    Front End Application
    * Includes the basic structure of front end
    * Includes the server that provides the html file

---
+++ FrontEnd/app/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var path = require("path");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontView.html'))
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
