62ae201d4598ed4bf3014a6b0ffb858dd20fb29c
Mark Sansome <marksansome11@gmail.com>
2018-11-20 10:51:25 -0500

    setting up passport and express

---
+++ api/app.js
@@ -0,0 +1,69 @@
+// Dependencies
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+port = process.env.PORT || 3000;
+
+// Includes
+var routes = require('./routes/index');
+var users = require('./routes/usersRoute');
+
+// Initalize
+app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', routes);
+
+// passport 
+var Account = require('./models/accountsModel');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// production error handler
+if (app.get('env') === 'production') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+} else {
+    // development error handler
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+module.exports = app;
+
+app.listen(port);
+console.log('OSS RESTful API server started on: ' + port);
