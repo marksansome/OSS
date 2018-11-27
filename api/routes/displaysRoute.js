c807cb79d4a45a139ff6fdda143f4e7f7f33aedf
Mark Sansome <marksansome11@gmail.com>
2018-11-27 13:19:35 -0500

    changing displays route

--- api/routes/displaysRoute.js
+++ api/routes/displaysRoute.js
@@ -6,30 +6,30 @@ const displaysController = require('../controllers/displaysController');
 const router = express.Router();
 
 /* Currently NOT FINISHED */
-router.post('/createDisplay', (req, res, next) => {
- 
+router.post('/displays', (req, res, next) => {
+
     if (req.user == null) {
         res.status(500).send("Error - cannot POST /createDisplay: no valid account found. Please sign in first.");
-    } else {        
-        let user = req.user; 
+    } else {
+        let user = req.user;
         let userID = user[0]["userID"];
         let displayName = user[0]["displayName"];
 
-        let newDisplay = req.body["display"]; 
+        let newDisplay = req.body["display"];
 
         if (newDisplay) {
-      
+
             console.log("create display with userID" + userID);
             let response = displaysController.createDisplay(userID, newDisplay).then((result) => {
                 res.status(200).send(result);
-            }); 
+            });
         }
         else {
-             res.status(500).send("Error: No 'display' key (Which should contain all display info) found in body of request for /createDisplay");
-         }
+            res.status(500).send("Error: No 'display' key (Which should contain all display info) found in body of request for /createDisplay");
+        }
 
-         
+
     }
-}); 
+});
 
 module.exports = router;
