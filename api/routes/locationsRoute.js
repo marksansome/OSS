0f49aaf861afdf1aae19a52181f9729f892c26f0
Mark Sansome <marksansome11@gmail.com>
2018-11-27 13:17:28 -0500

    fixing location routes

--- api/routes/locationsRoute.js
+++ api/routes/locationsRoute.js
@@ -6,16 +6,16 @@ const locationsController = require('../controllers/locationsController');
 const router = express.Router();
 
 router.get('/locations', (req, res, next) => {
-    
+
     if (req.user == null) {
         res.status(500).send("Error - cannot GET /locations: no valid account found. Please sign in first.");
     } else {
-        let user = req.user; 
+        let user = req.user;
         let userID = user[0]["userID"];
         let displayName = user[0]["displayName"];
 
-        let locationsJson = locationsController.getLocations(userID).then((result)=> {
-          
+        let locationsJson = locationsController.getLocations(userID).then((result) => {
+
             console.log("Result = " + locationsJson);
 
             res.status(200).send(result);
@@ -23,94 +23,59 @@ router.get('/locations', (req, res, next) => {
     }
 });
 
-/* It will only get the location with the ID that you provide if your AccountID matches that of the location*/
-router.get('/locationWithId', (req, res, next) => { 
+router.post('/locations', (req, res, next) => {
 
-     if (req.user == null) {
-        res.status(500).send("Error - cannot GET /locationWithID: no valid account found. Please sign in first.");
+    if (req.user == null) {
+        res.status(500).send("Error - cannot POST /createLocation: no valid account found. Please sign in first.");
     } else {
-        let user = req.user; 
+        let user = req.user;
         let userID = user[0]["userID"];
         let displayName = user[0]["displayName"];
 
-        let locationID = req.body["location_id"];
+        let newLocation = req.body["locations"];
+
+        if (newLocation) {
+
+            console.log("create location with user ID" + userID);
+            let response = locationsController.createLocation(userID, newLocation).then((result) => {
+                res.status(200).send(result);
+            });
+        }
+        else {
+            res.status(500).send("Error: No 'locations' key found in body of request for /createlocation");
+        }
+    }
+});
+
+/* It will only get the location with the ID that you provide if your AccountID matches that of the location*/
+router.get('/locations/:id', (req, res, next) => {
+
+    if (req.user == null) {
+        res.status(500).send("Error - cannot GET /locationWithID: no valid account found. Please sign in first.");
+    } else {
+        let user = req.user;
+        let userID = user[0]["userID"];
+        let displayName = user[0]["displayName"];
+
+        let locationID = req.param.id;
         console.log(locationID);
 
         /* If the API caller provided a locationID in the body */
-        if (locationID)  { 
-            let LocationJson = locationsController.getLocationWithID(userID, locationID).then((result)=> {
-             
+        if (locationID) {
+            let LocationJson = locationsController.getLocationWithID(userID, locationID).then((result) => {
+
                 console.log("Result = " + LocationJson);
 
                 res.status(200).send(result);
             });
 
-        } 
+        }
         /* If the API caller DID NOT provide a locationID in the body */
-        else { 
-             res.status(500).send("Error: No 'location_id' key found in body of request for /locationWithID");
-        }
-
-    }
-});
-
-router.post('/createLocation', (req, res, next) => {
-
-    if (req.user == null) {
-        res.status(500).send("Error - cannot POST /createLocation: no valid account found. Please sign in first.");
-    } else {
-
-        let user = req.user; 
-        let userID = user[0]["userID"];
-        let displayName = user[0]["displayName"];
-
-        let newLocation = req.body["locations"]; 
-
-        if (newLocation) {
-      
-            console.log("create location with user ID" + userID);
-            let response = locationsController.createLocation(userID, newLocation).then((result) => {
-                res.status(200).send(result);
-            }); 
-        }
         else {
-             res.status(500).send("Error: No 'locations' key found in body of request for /createlocation");
-         }
+            res.status(500).send("Error: No 'location_id' key found in body of request for /locationWithID");
+        }
+
     }
-}); 
-
-
-
-//Hit like http://localhost:3000/api/getLocation/your_id_here
-router.get('/getLocation/:id', (req, res, next) => {
-
-	let id = req.params.id; console.log(id);
-	
-
-
-    res.status(200).send("locations API hit");
 });
-
-
-
 
 module.exports = router;
-
-/* 
-
-'use strict';
-module.exports = function (app) {
-    var locations = require('../controllers/locationsController');
-
-    // todoList Routes
-    app.route('/locations')
-        .get(locations.list_all_tasks)
-        .post(locations.create_a_task);
-
-
-    app.route('/locations/:locationId');
-    // .get(locations.read_a_task)
-    // .put(locations.update_a_task)
-    // .delete(locations.delete_a_task);
-};
-*/ 
